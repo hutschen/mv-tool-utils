e8246cc127240948126d983504a4e7fb3c4639c7
Helmar Hutschenreuter <hutschen.mail@posteo.de>
2022-12-22 12:09:54 +0100

    rename project to mv-tool utilities

--- mvtool_utils/api.py
+++ mvtool_utils/api.py
@@ -2,18 +2,18 @@
 #
 # Copyright (C) 2022 Helmar Hutschenreuter
 #
-# MV-Tool Import Helper is free software: you can redistribute it and/or modify
+# MV-Tool Utilities is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# MV-Tool Import Helper is distributed in the hope that it will be useful,
+# MV-Tool Utilities is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Lesser General Public License for more details.
 #
 # You should have received a copy of the GNU Lesser General Public License
-# along with MV-Tool Import Helper. If not, see <http://www.gnu.org/licenses/>.
+# along with MV-Tool Utilities. If not, see <http://www.gnu.org/licenses/>.
 
 import json
 import ssl
